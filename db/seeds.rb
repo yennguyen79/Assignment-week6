03315a0bcbb06c2288d0b3df1588a4f3e5aee9a1
yennguyen79 <yen.nnh794@gmail.com>
2017-08-17 11:07:33 +0700

    can change avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.generate_users(5, "male")
+User.generate_users(5, "female")User.generate_users(5, "male")
 User.generate_users(5, "female")
