d569b119f6b65d2a7ed4d5afa63327cf957f7ba3
yennguyen79 <yen.nnh794@gmail.com>
2017-08-17 16:56:46 +0700

    commit before do email

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
-end
+  
+  def index
+    @posts = Post.all.order("updated_at DESC")
+  end
+ 
+  def new
+  end
+ 
+  def create
+    @post = Post.new post_params
+    @post.poster = current_user
+    if @post.save
+      flash[:success] = "Success in creating post"
+    else
+      flash[:error] = "#{@post.errors.full_messages.to_sentence}"
+    end
+    redirect_to posts_path  
+  end
+ 
+  def post_params
+    params.require(:post).permit(:body)
+  end
+ 
+ end
